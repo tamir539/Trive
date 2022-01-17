71521dd8cfe6616d443463a3cff5c0fc916b9529
tamir <tamir.burstein@gmail.com>
2022-01-17 12:30:33 +0200

    finish sprotocol

--- server/sprotocol.py
+++ server/sprotocol.py
@@ -4,7 +4,23 @@ def create_all_files_msg(msg):
     :param msg:string that presents all the files of user in structure
     :return: builds and return the msg by the protocol
     '''
-    pass
+    return create_response('04', msg)
+
+def unpack_msg(msg):
+    '''
+
+    :param msg:msg from the client
+    :return: unpack by the protocol and returns to the main server the msg and the relevants args
+    '''
+    command_by_code = {'02': 'login', '03': 'register', '04': 'send_all_files', '05': 'upload',
+                       '06':'download', '07': 'delete', '08': 'add_to_folder', '09': 'create_folder',
+                       '10': 'change_details', '11': 'share', '13': 'change_name', '14': 'forgot_password'}
+    #the code of the msg
+    code = msg[:2]
+    msg = msg[2:]
+    #all the arguments of the msg
+    args = msg.split('&')
+    return command_by_code[code], args
 
 def create_download_response_msg(file_len, port):
     '''
@@ -13,14 +29,8 @@ def create_download_response_msg(file_len, port):
     :param port: port for the download server
     :return: builds and return the msg by the protocol
     '''
-    pass
-
-def unpack_msg(msg):
-    '''
-
-    :param msg:msg from the client
-    :return: unpack by the protocol and returns to the main server the msg and the relevants args
-    '''
+    msg = port + '&' + file_len
+    return create_response('06', msg)
 
 def create_login_response_msg(msg):
     '''
@@ -28,7 +38,7 @@ def create_login_response_msg(msg):
     :param msg:string that describes response to login
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('02', msg)
 
 def create_register_response_msg(msg):
     '''
@@ -36,7 +46,7 @@ def create_register_response_msg(msg):
     :param msg:string that describes response to register
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('03', msg)
 
 def create_upload_file_response_msg(msg):
     '''
@@ -44,7 +54,7 @@ def create_upload_file_response_msg(msg):
     :param msg:string that describes response to upload
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('05', msg)
 
 def create_delete_file_response_msg(msg):
     '''
@@ -52,7 +62,7 @@ def create_delete_file_response_msg(msg):
     :param msg:string that describes response to the delete
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('07', msg)
 
 def create_insert_file_to_folder_response_msg(msg):
     '''
@@ -60,7 +70,7 @@ def create_insert_file_to_folder_response_msg(msg):
     :param msg:string that describes response to insert the file into folder
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('08', msg)
 
 def create_create_folder_response_msg(msg):
     '''
@@ -68,7 +78,7 @@ def create_create_folder_response_msg(msg):
     :param msg:string that describes response to create folder
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('09', msg)
 
 def create_change_detail_response_msg(msg):
     '''
@@ -76,7 +86,7 @@ def create_change_detail_response_msg(msg):
     :param msg:string that describes response to change details
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('10', msg)
 
 def create_share_file_response_msg(msg):
     '''
@@ -84,7 +94,7 @@ def create_share_file_response_msg(msg):
     :param msg:string that describes response to share file or folder
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('11', msg)
 
 def create_change_file_name_response_msg(msg):
     '''
@@ -92,4 +102,20 @@ def create_change_file_name_response_msg(msg):
     :param msg:string that describes response to change files or folders name
     :return:build and return the msg by the protocol
     '''
-    pass
+    return create_response('13', msg)
+
+def create_response(code, msg):
+    '''
+
+    :param code:code of the response by protocol
+    :param msg:msg to the client
+    :return:create and return response msg by the protocol
+    '''
+
+    msg = code + msg
+    return msg
+
+
+if __name__ == '__main__':
+    msg = input('enter massage to unpack: ')
+    print(unpack_msg(msg))
